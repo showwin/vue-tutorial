393f0210c61396417ad6438866b1a3f6478d1f6e
showwin <showwin_kmc@yahoo.co.jp>
2018-01-16 08:53:56 +0900

    [add] 2018/01/16 学習分

---
+++ 2_transitions_and_animation/transitioning_state/index.js
@@ -0,0 +1,27 @@
+new Vue({
+  el: '#animated-number-demo',
+  data: {
+    number: 0,
+    animatedNumber: 0
+  },
+  watch: {
+    number: function(newValue, oldValue) {
+      var vm = this
+      function animate () {
+        if (TWEEN.update()) {
+          requestAnimationFrame(animate)
+        }
+      }
+
+      new TWEEN.Tween({ tweeningNumber: oldValue })
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .to({ tweeningNumber: newValue }, 500)
+        .onUpdate(function() {
+          vm.animatedNumber = this.tweeningNumber.toFixed(0)
+        })
+        .start()
+
+      animate()
+    }
+  }
+})
